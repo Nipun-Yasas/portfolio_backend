5e9cf4a2ac1aebedd1fb62b1c260a5dcee07e1c6
Bandara O.P.N.Y.K <bandaraopnyk.22@uom.lk>
2024-05-05 20:36:47 +0530

    new schema blog added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5000;
 
 require('dotenv').config();
 const Project = require('./Project');
+const Blog = require('./Blog');
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -15,6 +16,15 @@ app.get('/projects', async (req, res) => {
     try {
         const projects = await Project.find();
         res.json(projects);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.get('/blogs', async (req, res) => {
+    try {
+        const blogs = await Blog.find();
+        res.json(blogs);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
